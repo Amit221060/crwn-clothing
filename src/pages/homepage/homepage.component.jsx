6609745e37c7d970613c3f41455db2e73194ef6d
Amit Singh <amsing52@in.ibm.com>
2020-06-27 15:16:37 +0530

    broken home compoent to smallar component and added style to menu-item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./homepage.style.scss"
+import '../../components/directort-item/directory.component'
+
+import Directory from '../../components/directort-item/directory.component'
+
+const HomePage = ()=>(
+<div className="homePage">
+        <Directory/>     
+</div>
+    
+);
+
+export default HomePage;
